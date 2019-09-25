87348fdbf0b0a3a2d191f3f99456dec1b4f5c60c
kol19pl <kol19pl@gmail.com>
2019-09-25 18:45:07 +0200

    apdejt

--- app/src/main/java/com/koltech/kol/generatortypwlotto/MainActivity.java
+++ app/src/main/java/com/koltech/kol/generatortypwlotto/MainActivity.java
@@ -7,6 +7,9 @@ import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 import java.util.Random;
 
@@ -47,7 +50,11 @@ public class MainActivity extends AppCompatActivity {
         piec=(TextView)findViewById(R.id.piontaliczba);
         szesc=(TextView)findViewById(R.id.szustaliczba);
 
-
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
 
         mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
